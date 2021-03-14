b3219b4f5379dd224375da878929cc276c332377
GabrielPradoC <gabriel_pradojs@hotmail.com>
2021-03-13 22:28:55 -0300

    Rafactored code to work only with youtube.

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 // *TODO*
 // criar um comando de queue que mostre o título e duração da música com link (embed) (50% done)
 // mudar o nome do bot
-// testar se ele pausa/"despausa" a música em mais de 1 servidor (tenho quase ctz que n)
-// colocar remove nas músicas da queue
+
 const Discord = require('discord.js');
 
 const {
 	prefix,
-	token,
-	spotify_client_id,
-	spotify_client_secret,
-	youtube_api_key
+	token
 } = require('./config.json');
 
 const ytdl = require('ytdl-core');
 
-const axios = require('axios');
-
-const google = require('googleapis');
-
-const youtube = new google.youtube_v3.Youtube({
-	version: 'v3',
-	auth: youtube_api_key
-})
-
 const client = new Discord.Client();
-
-const vidData = require('vid_data');
 
 const ytpl = require('ytpl');
 
@@ -40,73 +25,83 @@ client.on('reconnecting', () => console.log('Reconnecting!'));
 
 client.on('disconnect', () => console.log('Disconnect!'));
 
-client.on('unhandledRejection', error => console.log('Unhandled promise rejection:'+ error));
 
-
-client.on('message', async message => {
+client.on('message', message => {
 	if (message.author.bot || !message.content.startsWith(prefix)) return;
 	let argsa = message.content.slice(prefix.length).trim().split(/ +/);
-
 	const serverQueue = queue.get(message.guild.id);
-
-	if (message.content.startsWith(`${prefix}play`)) {
-		argsa.shift();
-		inputHandler(message, argsa.join(' '), serverQueue);		
-	} else if (message.content.startsWith(`${prefix}skip`)) {
-		skip(message, serverQueue);
-	} else if (message.content.startsWith(`${prefix}stop`)) {
-		stop(message, serverQueue);
-		return;
-	} else if (message.content.startsWith(`${prefix}queue`)) {
-		queueDisplayHandler(message, serverQueue.songs);
-	} else if (message.content.startsWith(`${prefix}pause`)) {
-		pause(message, serverQueue);
-	} else if (message.content.startsWith(`${prefix}resume`)) {
-		resume(message, serverQueue);
-	} else if(message.content.startsWith(`${prefix}np`)){
-		message.channel.send(`Now playing: **${serverQueue.nowPlaying}**`);
-	}else if(message.content.startsWith(`${prefix}remove`)){
-		if(argsa[1] <= 0 || !argsa[1]) return message.channel.send(`Invalid index.`);
-		serverQueue.songs = removeMusic(serverQueue, message, argsa[1]-1);
+	switch(argsa[0]){
+		case 'play':
+			argsa.shift();
+			inputHandler(message, argsa.join(' '), serverQueue);	
+			break;
+		case 'skip':
+			skip(message, serverQueue);
+			break;
+		case 'stop':
+			stop(message, serverQueue);
+			break;
+		case 'queue':
+			argsa.shift();
+			queueDisplayHandler(message, serverQueue.songs, argsa[0]);
+			break;
+		case 'pause':
+			pause(message, serverQueue);
+			break;
+		case 'resume':
+			resume(message, serverQueue);
+			break;
+		case 'np':
+			nowPlaying(message, serverQueue);
+			break;
+		case 'remove':
+			if (!serverQueue) return message.channel.send('There\'s no songs in the queue.');
+			if(argsa[1] <= 0 || !argsa[1] || argsa[1] > serverQueue.songs.length || isNaN(argsa[1])) return message.channel.send(`Invalid index.`);
+			serverQueue.songs = removeMusic(serverQueue, message, argsa[1]-1);
+			break;
 	}
 });
 
+
+
 async function inputHandler(message, argsa, serverQueue) {
 	const voiceChannel = message.member.voice.channel;
-		if (!voiceChannel) message.channel.send('You need to be in a voice channel to play music!');
+		if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+		let isPaused = false;
+		if(serverQueue) isPaused = serverQueue.connection.dispatcher.paused;		
 		const permissions = voiceChannel.permissionsFor(message.client.user);
-		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) message.channel.send('I need the permissions to join and speak in your voice channel!');
+		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) return message.channel.send('I need the permissions to join and speak in your voice channel!');
 		let list = [];
-			let messageArgs = argsa;
-			if(messageArgs.includes('playlist?list')){
-				const playid = await ytpl(messageArgs)
+			if(argsa.includes('playlist?list')){
+				const playid = await ytpl(argsa)
 				let { items } = playid;
 				for(const obj of items){
 					list.push(obj);
 				};
 				linkHandler(message, list, serverQueue, queue, voiceChannel);
-			}else if(messageArgs.includes('https://www.youtube.com/watch?v=')){
-				const singleVideoInfo = await ytdl.getInfo(messageArgs);
+			}else if(argsa.includes('https://www.youtube.com/watch?v=')){
+				const singleVideoInfo = await ytdl.getInfo(argsa);
 				list.push(singleVideoInfo.videoDetails);
 				linkHandler(message, list, serverQueue, queue, voiceChannel);
-			}else if(messageArgs.includes('https://open.spotify.com/')){			
-				spotifyLinkHandler(messageArgs, message, serverQueue);
+			}else if(isPaused){
+				resume(message, serverQueue);
 			}else{
 				message.channel.send('Invalid arguments, need to be either a youtube playlist link or a video link.');
-				return;
 			}		
 }
 
 function skip(message, serverQueue) {
-	if (!message.member.voice.channel) message.channel.send('You have to be in a voice channel to skip the music!');
-	if (!serverQueue) message.channel.send('There is no song that I could skip!');
+	if (!serverQueue) return message.channel.send('There is no song to be skipped!');
+	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to skip the music!');
+	if(message.member.voice.channel.id != serverQueue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to skip the music.');
 	serverQueue.songs.shift();
 	play(message.guild, serverQueue.songs[0], message);
 }
 
-
 function stop(message, serverQueue) {
-	if (!message.member.voice.channel) message.channel.send('You have to be in a voice channel to stop the music!');
+	if(!serverQueue) return message.channel.send('There is no songs to stop.');
+	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if(message.member.voice.channel.id != serverQueue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to stop the music.');
 	serverQueue.songs = [];
 	serverQueue.connection.dispatcher.destroy();
 	serverQueue.songs.shift();
@@ -114,18 +109,27 @@ function stop(message, serverQueue) {
 }
 
 function pause(message, serverQueue){
-	serverQueue.connection.dispatcher.pause();
+	if(!serverQueue) return message.channel.send('There is no music to pause.');
+	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to pause the music!');
+	if(message.member.voice.channel.id != serverQueue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to pause the music.');
+	serverQueue.connection.dispatcher.pause(true);
 	return message.channel.send('Music paused.');
 };
 
 function resume(message, serverQueue){
-	serverQueue.connection.dispatcher.resume();
-	return message.channel.send('Music resumed.');
+	if(!serverQueue) return message.channel.send('There is no music to resume.');
+	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to resume the music!');
+	if(message.member.voice.channel.id != serverQueue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to resume the music.');
+	if(serverQueue.connection.dispatcher.paused){
+		serverQueue.connection.dispatcher.resume();
+		return message.channel.send('Music resumed.');
+	}else{
+		message.channel.send('Music isn\'t paused.')
+	}	
 };
 
-async function play(guild, song, message) {
+function play(guild, song, message) {
 	const serverQueue = queue.get(guild.id);
-
 	if (!song) {
 		serverQueue.voiceChannel.leave();
 		message.channel.send('Queue is empty, leaving the voice channel.');
@@ -133,42 +137,41 @@ async function play(guild, song, message) {
 		return;
 	}
 
-	const dispatcher = serverQueue.connection.play(ytdl(song.url)/* .on('response', debug=> console.log(debug.statusCode)).on('error', debug=> console.log('Response : '+debug)).on('info', debug=> console.log(debug.title)) */)
+	const dispatcher = serverQueue.connection.play(ytdl(song.url))
 		.on('finish', () => {
-			console.log('Music ended!');
 			serverQueue.songs.shift();
 			play(guild, serverQueue.songs[0], message);
-		})
-		.on('debug', debug=> {
-			console.log(debug);
 		})
 		.on('error', error => {
 			console.error(error);
 		});
 
-	message.channel.send(`Now playing: **${song.title}** | **${timeParser(song.duration)}**`);
+	message.channel.send(`Now playing: **${song.title}** | **${song.duration}**`);
 	serverQueue.nowPlaying = song.title;
 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 }
 
 async function linkHandler(message, list, serverQueue,queue, voiceChannel){
+	if(serverQueue && message.member.voice.channel.id != serverQueue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to add new songs.');
 	if (!serverQueue) {
 		const queueConstruct = {
 			textChannel: message.channel,
 			voiceChannel: voiceChannel,
+			requestedBy: message.author,
 			connection: null,
 			nowPlaying: null,
+			nowPlayingTimeLeft: null,
 			songs: [],
 			volume: 3,
 			playing: true,
 		};
 		for(const item of list){
 			item.url = item.shortUrl ? item.shortUrl : item.video_url;
-			item.duration = item.durationSec ? item.durationSec : item.lengthSeconds;
+			item.duration = item.durationSec ? timeParser(item.durationSec) : timeParser(item.lengthSeconds);
 			queueConstruct.songs.push(item);
 		}
 		queue.set(message.guild.id, queueConstruct);
-		message.channel.send(`Added ${queueConstruct.songs.length} ${queueConstruct.songs.length>1? 'musics': 'music'} to the queue!`);
+		message.channel.send(`Added ${queueConstruct.songs.length} ${queueConstruct.songs.length>1? 'songs': 'song'} to the queue!`);
 		try {
 			let connection = await voiceChannel.join();
 			queueConstruct.connection = connection;
@@ -183,24 +186,33 @@ async function linkHandler(message, list, serverQueue,queue, voiceChannel){
 		let textToDisplay = list.length >= 2? `${list.length} songs added to the queue!` : `**${list[0].title}** has been added to the queue!`;
 		for(const item of list){
 			item.url = item.shorturl ? item.shorturl : item.video_url;
-			item.duration = item.durationSec ? item.durationSec : item.lengthSeconds;
+			item.duration = item.durationSec ? timeParser(item.durationSec) : timeParser(item.lengthSeconds);
 			serverQueue.songs.push(item);
 		}
 		message.channel.send(textToDisplay);
 	}
 }
 
-async function queueDisplayHandler(message, queue){
-	let responseMsg = [];
-	let iterable = 1;
-	for(const item of queue){
-		responseMsg.push(`**${iterable} - ${item.title}**\n`);
-		iterable++;
+function queueDisplayHandler(message, queue, argsPage = 1){
+	const queueLength = queue.length;
+	const maxSongsToDisplay = 10;	
+	const maxPages = Math.ceil(queueLength/ maxSongsToDisplay);
+	if(argsPage> maxPages || argsPage<= 0 || isNaN(argsPage)) return message.channel.send('Invalid page number.');	
+	const startingPos = 10*(argsPage-1);	
+	let responseMsg = [];	
+	for(let i = 0;i<maxSongsToDisplay;i++){
+			if(!queue[startingPos+i]){ break };
+			responseMsg.push(`**${startingPos+i+1} - ${queue[startingPos+i].title} | ${queue[startingPos+i].duration}** \n`);
+	}
+	if(queueLength> maxSongsToDisplay){
+		responseMsg.push(`Page ${argsPage} of ${maxPages}`);
 	}
 	message.channel.send(responseMsg.join(''));
 }
 
 function removeMusic(queue, message, args){
+	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to remove a song!');
+	if(message.member.voice.channel.id != queue.voiceChannel.id) return message.channel.send('You need to be in the same voice channel as the bot to remove a song.');
 	let removedSong = queue.songs.splice(args, 1);
 	message.channel.send(`Removed **${removedSong[0].title}** from the queue.`);
 	if(removedSong[0].title === queue.nowPlaying){
@@ -220,128 +232,10 @@ function str_pad_left(string,pad,length) {
     return (new Array(length+1).join(pad)+string).slice(-length);
 }
 
-async function spotifySingleTrackHandler(spotifyID, message, queue){
-	const authResponse = await axios({
-		method: 'post',
-		url: 'https://accounts.spotify.com/api/token',
-		data: 'grant_type=client_credentials',
-		headers: {
-			'Authorization': 'Basic ' + (Buffer.from(`${spotify_client_id}:${spotify_client_secret}`).toString('base64')),
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		json: true
-	})
-	.catch(err=>{
-		console.log(err);
-	})
-
-	if(authResponse.status === 200){
-		const dataResponse = await axios({
-			method: 'get',
-			url: `https://api.spotify.com/v1/tracks/${spotifyID}`,
-			headers: {
-				'Authorization': `Bearer ${authResponse.data.access_token}`,
-			},
-			json: true
-		})
-		const spotifyData = dataResponse.data;
-		const musicName = `${spotifyData.name}`;
-		ytSearch(musicName, message, queue);		
-	}else{
-		message.channel.send('Couldn\'t fetch the track, try again later');
-		console.log(authResponse.status+' An error ocurred while fetching a track!');
-	}
-}
-
-async function spotifyPlaylistHandler(playlistId, message, serverQueue){
-	
-		const authResponse = await axios({
-		method: 'post',
-		url: 'https://accounts.spotify.com/api/token',
-		data: 'grant_type=client_credentials',
-		headers: {
-			'Authorization': 'Basic ' + (Buffer.from(`${spotify_client_id}:${spotify_client_secret}`).toString('base64')),
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		json: true
-	}).catch((err)=>{
-		console.log(authResponse.status+' '+err);
-	})
-
-
-	if(authResponse.status === 200){
-			const dataResponse = await axios({
-			method: 'get',
-			url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-			params: {
-				market: 'BR',
-				fields: 'items(track(name%2C%20artists(name)))',
-				limit: 99
-			},
-			headers: {
-				'Authorization': `Bearer ${authResponse.data.access_token}`,
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
-			},
-			json: true
-		}).catch((err)=>{
-			console.log(dataResponse.status+' '+err);
-		});
-		const spotifyData = dataResponse.data;
-		const trackList = [];
-		console.log(spotifyData.items.length);
-		for(const track of spotifyData.items){
-			let searchRes = ytSearch(track.track.name, message, serverQueue, 1)
-			trackList.push(searchRes);
-		}
-		console.log(trackList);
-		return;
-		const voiceChannel = message.member.voice.channel;
-		if (!voiceChannel) message.channel.send('You need to be in a voice channel to play music!');
-		const permissions = voiceChannel.permissionsFor(message.client.user);
-		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) message.channel.send('I need the permissions to join and speak in your voice channel!');
-		linkHandler(message, trackList, serverQueue, queue, voiceChannel);
-	}else{
-		message.channel.send('Couldn\'t fetch the playlist, try again later');
-		console.log(authResponse.status+' An error ocurred while fetching a track!');
-	}
-}
-
-/////////////////////////////////////////////  need to change this later
-
-function ytSearch(searchString, message,  queue, shouldReturn = 0){
-	let results = youtube.search.list({
-		part: 'snippet',
-		q: searchString,
-		maxResults: 1
-	}, async function(err, data){
-		if(err) console.log(err);
-		if(data){
-			let ytSearchQ = await axios({
-				method: 'get',
-				url: data.request.responseURL,
-				json: true
-			})
-			const video = ytSearchQ.data.items;
-			let string = `https://www.youtube.com/watch?v=${video[0].id.videoId}`;
-			console.log(string)
-			if(shouldReturn === 1){				
-				return string;
-			}	
-			inputHandler(message, string, queue);
-		}
-	})
-}
-
-///////////////////////////////////////////////////
-function spotifyLinkHandler(link, message, queue){
-	if(link.includes('/track/')){
-	let trackId = link.slice(link.lastIndexOf('track/')+6, link.indexOf('?si'));
-		spotifySingleTrackHandler(trackId, message, queue);
-	}else if(link.includes('/playlist/')){
-		let playlistId = link.slice(link.lastIndexOf('playlist/')+9, link.indexOf('?si'));
-		spotifyPlaylistHandler(playlistId, message, queue)
-	}
+function nowPlaying(message, serverQueue){
+	if(!serverQueue) return message.channel.send('Nothing playing.');
+	serverQueue.nowPlayingTimeLeft = (serverQueue.connection.dispatcher.streamTime - serverQueue.connection.dispatcher.pausedTime) /1000;
+	message.channel.send(`Now playing: **${serverQueue.nowPlaying}** | ${timeParser(serverQueue.nowPlayingTimeLeft.toFixed(0))}/${serverQueue.songs[0].duration}`);
 }
 
 client.login(token);
